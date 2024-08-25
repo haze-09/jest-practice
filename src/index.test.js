2ed3335f07e01c2802e798cf0e7426774009f6ad
haze-09 <68776764+haze-09@users.noreply.github.com>
2024-08-25 09:30:56 +0530

    inital file setup

--- src/index.test.js
+++ src/index.test.js
@@ -1,7 +1,2 @@
-import {test, expect} from "@jest/globals"
+import {describe, expect, test} from '@jest/globals'
 
-import sum from "./index.js";
-
-test('adds 1 + 2 to equal 3', () => {
-    expect(sum(1, 2)).toBe(3);
-  });
